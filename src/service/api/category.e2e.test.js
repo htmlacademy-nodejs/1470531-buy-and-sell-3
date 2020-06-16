fdd043110aed09e85ba2e97b7468ce3ac1c46039
Alexey Ivanov <4985900@gmail.com>
2020-06-16 16:25:19 +0300

    feat: tests for api/category

--- src/service/api/category.e2e.test.js
+++ src/service/api/category.e2e.test.js
@@ -3,11 +3,22 @@
 const request = require(`supertest`);
 const {createApp} = require(`../cli/server`);
 
-describe(`Categories`, () => {
-  test(`category test`, async () => {
-    const server = await createApp();
+let server;
+
+beforeAll(async () => {
+  server = await createApp();
+});
+
+describe(`Categories API end-points`, () => {
+  test(`Should return status 200 on GET request`, async () => {
     const res = await request(server).get(`/api/category`);
 
     expect(res.statusCode).toBe(200);
+  });
+
+  test(`Should have at least one category`, async () => {
+    const res = await request(server).get(`/api/category`);
+
+    expect(res.body.length).toBeGreaterThan(0);
   });
 });
