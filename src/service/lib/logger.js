a957728bc60b11991c25a4926cb762c60c494010
Ivanov Alexey <4985900@gmail.com>
2020-06-24 13:41:59 +0300

    feat: save logs to file

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const logger = require(`pino`)({
-  name: `pino-and-express`,
-  level: process.env.LOG_LEVEL || `debug`
+const fs = require(`fs`);
+const pinoms = require(`pino-multi-stream`);
+
+const prettyStream = pinoms.prettyStream({
+  prettyPrint: {
+    colorize: true,
+    translateTime: `SYS:standard`,
+    ignore: `hostname,pid`
+  },
+  prettifier: require(`pino-pretty`)
 });
+const streams = [
+  {stream: fs.createWriteStream(`./src/service/logs/logs.txt`)},
+  {stream: prettyStream}
+];
+
+
+const logger = pinoms({
+  name: `pino-and-express`,
+  level: process.env.LOG_LEVEL || `debug`,
+}, pinoms.multistream(streams));
 
 module.exports = {
   logger,
